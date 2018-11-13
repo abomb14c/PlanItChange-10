e80ef4358f0fc70eeff573d295a53b07d034a4b6
Alan Charles <alanjcharles14@gmail.com>
2018-11-13 15:00:13 -0700

    Add login structure

---
+++ src/components/Login/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="create-user-container">
+        {/* <CreateUser /> */}
+      </div>
+      <div className="login-user-container">
+        {/* <LoginUser /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Login; 
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import { Route, Redirect, Switch, withRouter } from 'react-router-dom';
 import './App.css';
+
+import { Route, Redirect, Switch, withRouter } from 'react-router-dom';
+
+import Login from '../../components/Login/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <Switch>
+          <Route
+            exact path= "/login"
+            render={() => (
+              this.props.user.user_id ?
+                <Redirect to="/" /> :
+                <Login />
+            )}
+          />
+          </Switch>
       </div>
     );
   }
