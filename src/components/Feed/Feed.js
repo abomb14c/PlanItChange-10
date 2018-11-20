247abf0a6cfd3a36ff2368692cf75fc6d97c0c08
Alan Charles <alanjcharles14@gmail.com>
2018-11-19 17:29:29 -0700

    set up structure for newsfeed

---
+++ src/components/Feed/Feed.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const Feed = (props) => {
+  
+
+}
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './home.css';
 
 import PostForm from '../../containers/PostForm/PostForm';
+import { Feed } from '../Feed/Feed';
 
-const Home = (props) => {
+class Home extends Component  {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      feed: ''
+    }
+  }
+
+  componentDidMount = () => {
+
+  }
+  render() {
   return (
     <div className="home">
       <div className="current-locaton-container">
         <p className="current-location-text">Updates for Denver</p>
       </div>
       <PostForm />
+      {this.state.feed.length ? 
+        <Feed />:
+        null}
     </div>
   )
+}
 }
 
 export default Home; 
