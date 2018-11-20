f73105932a0623803205d8fadff3c24fb3eb1402
Alan Charles <alanjcharles14@gmail.com>
2018-11-20 12:12:50 -0700

    Add like button

--- src/containers/Post/Post.js
+++ src/containers/Post/Post.js
@@ -18,6 +18,11 @@ class Post extends Component {
           <h1 className="post-title">{this.props.post.title}</h1>
           <p className="post-desc">{this.props.post.desc}</p>
         </div>
+        <div className="interact-container">
+          <div className="like-container">
+            <div className="like-icon"></div>
+          </div>
+        </div>
       </div>
     )
   }
